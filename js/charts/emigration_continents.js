be1b8c027ad4f23739014695224e781ac6072fab
elnase <elena.seydel@gmail.com>
2016-07-28 14:02:33 +0200

    Update emigration_continents.js

--- js/charts/emigration_continents.js
+++ js/charts/emigration_continents.js
@@ -1,7 +1,10 @@
-var httpc = new XMLHttpRequest(); // simplified for clarity
-var url = "Test_Server.php";
-httpc.open("GET", url + "?key=Irak_Auswanderung_Kontinente", true);
-httpc.setRequestHeader("Content-Type", "application/json");
+//var httpc = new XMLHttpRequest(); // simplified for clarity
+//var url = "Test_Server.php";
+//httpc.open("GET", url + "?key=Irak_Auswanderung_Kontinente", true);
+//httpc.setRequestHeader("Content-Type", "application/json");
+
+var data = 
+{"JAHR":"1990","Afrika_prozentual":"0.50","Asien_prozentual":"90.16","Europa_prozentual":"5.81","LatinAmerica_prozentual":"0.02","Northern_America_prozentual":"3.45","Oceanien_prozentual":"0.06"}, {"JAHR":"1995","Afrika_prozentual":"2.81","Asien_prozentual":"79.05","Europa_prozentual":"11.69","LatinAmerica_prozentual":"0.02","Northern_America_prozentual":"6.30","Oceanien_prozentual":"0.14"}, {"JAHR":"2000","Afrika_prozentual":"5.77","Asien_prozentual":"62.62","Europa_prozentual":"18.88","LatinAmerica_prozentual":"0.03","Northern_America_prozentual":"10.01","Oceanien_prozentual":"2.69"}, {"JAHR":"2005","Afrika_prozentual":"5.21","Asien_prozentual":"61.25","Europa_prozentual":"21.15","LatinAmerica_prozentual":"0.04","Northern_America_prozentual":"9.45","Oceanien_prozentual":"2.91"}, {"JAHR":"2010","Afrika_prozentual":"3.38","Asien_prozentual":"71.03","Europa_prozentual":"16.84","LatinAmerica_prozentual":"0.03","Northern_America_prozentual":"6.48","Oceanien_prozentual":"2.25"}, {"JAHR":"2015","Afrika_prozentual":"6.53","Asien_prozentual":"41.33","Europa_prozentual":"31.84","LatinAmerica_prozentual":"0.06","Northern_America_prozentual":"15.60","Oceanien_prozentual":"4.63"};
 
 
 
@@ -14,13 +17,13 @@ function createChart(data) {
     var values5 = [];
     var values6 = [];
     for (var i = 0; i < data.length; i++) {
-        labels[i] = {"label": data[i].label};
-        values1[i] = {"value": data[i].afrika};
-        values2[i] = {"value": data[i].asien};
-        values3[i] = {"value": data[i].europa};
-        values4[i] = {"value": data[i].latin};
-        values5[i] = {"value": data[i].usa};
-        values6[i] = {"value": data[i].ozeanien};
+        labels[i] = {"label": data[i].JAHR};
+        values1[i] = {"value": data[i].Afrika_prozentual};
+        values2[i] = {"value": data[i].Asien_prozentual};
+        values3[i] = {"value": data[i].Europa_prozentual};
+        values4[i] = {"value": data[i].LatinAmerica_prozentual};
+        values5[i] = {"value": data[i].Northern_America_prozentual};
+        values6[i] = {"value": data[i].Oceanien_prozentual};
     }
     
     var vstrChart = new FusionCharts({
