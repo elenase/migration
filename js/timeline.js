14375d43499eb37a54a1cfbe70f2fdf5e04fc90a
elnase <elena.seydel@gmail.com>
2016-07-28 12:40:52 +0200

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -3,7 +3,7 @@
      width: "100%",
      height: "100%",
      background: "blue",
-     source: 'migration/timeline_history_iraq.json'
+     source: 'https://raw.githubusercontent.com/elnase/migration/gh-pages/timeline_history_iraq.json'
     }
 
 
