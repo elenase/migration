8e5543245462be0c23993b5dff4c7aa11d8dd692
elnase <elena.seydel@gmail.com>
2016-07-28 15:04:33 +0200

    Update immigration_and_emigration.js

--- js/charts/immigration_and_emigration.js
+++ js/charts/immigration_and_emigration.js
@@ -1,9 +1,10 @@
+/*
 var httpc1 = new XMLHttpRequest(); // simplified for clarity
 var url = "Test_Server.php";
 httpc1.open("GET", url + "?key=Ein_und_Abwanderung", true);
 httpc1.setRequestHeader("Content-Type", "application/json");
 
-/*
+
 function createChart() {
     // erstellt das Diagramm (ohne Daten)
 }
@@ -13,16 +14,22 @@ function setData(data) {
 }
 */
 
+
+var data =
+[{"Year":"1990","Total_Einwanderung":"83638","Total_Abwanderung":"-1501477","Netto_Migration":"-1417839"}, {"Year":"1995","Total_Einwanderung":"199460","Total_Abwanderung":"-1310112","Netto_Migration":"-1110652"}, {"Year":"2000","Total_Einwanderung":"210525","Total_Abwanderung":"-1161698","Netto_Migration":"-951173"}, {"Year":"2005","Total_Einwanderung":"132915","Total_Abwanderung":"-1474570","Netto_Migration":"-1341655"}, {"Year":"2010","Total_Einwanderung":"117389","Total_Abwanderung":"-2561482","Netto_Migration":"-2444093"}, {"Year":"2015","Total_Einwanderung":"353881","Total_Abwanderung":"-1479966","Netto_Migration":"-1126085"}];
+
+
+
 function createChart(data) {
     var labels = [];
     var values1 = [];
     var values2 = [];
     var values3 = [];
     for (var i = 0; i < data.length; i++) {
-        labels[i] = {"label": data[i].label};
-        values1[i] = {"value": data[i].einwanderung};
-        values2[i] = {"value": data[i].abwanderung};
-        values3[i] = {"value": data[i].nettoMigration};
+        labels[i] = {"label": data[i].Year};
+        values1[i] = {"value": data[i].Total_Einwanderung};
+        values2[i] = {"value": data[i].Total_Abwanderung};
+        values3[i] = {"value": data[i].Netto_Migration};
     }
     
     var Migration1Chart = new FusionCharts({
@@ -93,7 +100,7 @@ function createChart(data) {
             ]
         }
     }).render();
-    return data;
+/*    return data;
 } 
 
 
